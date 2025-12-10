4346f5a7f062111ed534c86908687bda1fe1d98e
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2025-12-10 19:28:52 +0100

    more

--- TrainResNet50.py
+++ TrainResNet50.py
@@ -6,17 +6,24 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import os
 
 DATA_DIR = "/Users/pepedesintas/Desktop/TFG/all-mias/outputData"
-IMG_SIZE = (224, 224)
-BATCH_SIZE = 16
-EPOCHS = 20
+IMG_SIZE = (224, 224) # we use this size, bc ResNet50 uses this standard size to train
+# permits the use of pretrained weights of ImageNet
+# lower computational cost
+BATCH_SIZE = 16 # quantity of images processed at a time
+EPOCHS = 20 # Not too much, as we are using transfer learning and it's already trained in ImageNet
+# If we train too much, we can produce overfitting -> Mias is small
 
+
+# We read images created in folders, and we assign them in order the name of the folder
+# it converts them in tensors (data save form in various dimensions)
+# Tensors allow the GPU/CPU to process all that data in parallel and very quickly, as it has many dimensions (heigh, channels, nº images...)
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     os.path.join(DATA_DIR, "train"),
     labels="inferred",
     label_mode="binary",
     image_size=IMG_SIZE,
     batch_size=BATCH_SIZE,
-    shuffle=True
+    shuffle=True # for a random train (more generalization)
 )
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -37,47 +44,68 @@ test_ds = tf.keras.preprocessing.image_dataset_from_directory(
     shuffle=False
 )
 
+# we save data in memory, so in every epoch we don't have to go back and search for the data again
+# .prefetch() -> prepares next batch while CNN is training
 AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().prefetch(AUTOTUNE)
 val_ds   = val_ds.cache().prefetch(AUTOTUNE)
 test_ds  = test_ds.cache().prefetch(AUTOTUNE)
 
-#* ---- DATA AUGMENTATION ----
-#data_augmentation = tf.keras.Sequential([
-#    layers.RandomFlip("horizontal"),
-#    layers.RandomRotation(0.05),
-#    layers.RandomZoom(0.10),
-#    layers.RandomContrast(0.15),
-#])
-#
 
-inputs = layers.Input(shape=(224,224,3))
-#x = data_augmentation(inputs)
-x = preprocess_input(inputs)
+# Artificially increases the dataset
+# it Helps the model generalize better
+# Reduces overfitting
+data_augmentation = tf.keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.05),
+    layers.RandomZoom(0.10),
+    layers.RandomContrast(0.15),
+])
+# in mammograms it is not always advisable to use horizontal flips and rotations, since they can alter the anatomical meaning.
 
+
+inputs = layers.Input(shape=(224,224,3)) # defines the way CNN receives the images (3 -> channel RGB)
+x = data_augmentation(inputs)
+x = preprocess_input(x) #normalizes using standard ResNet50 things
+
+
+# Convolutional part of the ResNet50 (all layers)
 base_model = ResNet50(include_top=False, weights="imagenet", input_tensor=x)
-base_model.trainable = False   # freeze before fine-tuning
+#Freezes the weights of ResNet50
+#They are not modified during training
+#Prevents overfitting (ideal with a small dataset)
+base_model.trainable = False
+# all up here is TRANSFER LEARNING
 
+
+# Reduces the feature maps to a vector → very efficient.
 x = layers.GlobalAveragePooling2D()(base_model.output)
+# Dense layer to learn more abstract patterns.
 x = layers.Dense(256, activation="relu")(x)
+# Randomly turns off 40% of neurons → prevents overfitting.
 x = layers.Dropout(0.4)(x)
+# Final output → probability of abnormality (binary).
 outputs = layers.Dense(1, activation="sigmoid")(x)
 
 model = models.Model(inputs, outputs)
 
 model.compile(
     optimizer=tf.keras.optimizers.Adam(1e-4),
-    loss="binary_crossentropy",
+    loss="binary_crossentropy", # loss function (binary-> 2 classes: normal, abnormal)
     metrics=["accuracy", tf.keras.metrics.AUC(name="auc")]
 )
 
 model.summary()
 
 callbacks = [
+    # Stops training if the AUC does not improve for 5 epochs → prevents overfitting.
     EarlyStopping(monitor="val_auc", patience=5, mode="max", restore_best_weights=True),
-    ModelCheckpoint("resnet50_mias3.h5", monitor="val_auc", mode="max", save_best_only=True)
+    #saves the best model according to validation
+    ModelCheckpoint("resnet50_mias_DA.h5", monitor="val_auc", mode="max", save_best_only=True)
 ]
 
+# -> TRAINING (the real learning)
+# only our final layers are trained
 history = model.fit(
     train_ds,
     validation_data=val_ds,
@@ -85,5 +113,5 @@ history = model.fit(
     callbacks=callbacks
 )
 
-print("Evaluating on test set:")
+print("-> Evaluating on test set:\n")
 model.evaluate(test_ds)
